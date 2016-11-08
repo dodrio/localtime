fb8b203263455d46a29e99e402de5c36f68d69cb
m31271n <m31271n@gmail.com>
2016-11-08 16:11:48 +0800

    Support specifying time manually

--- test.js
+++ test.js
@@ -3,4 +3,6 @@
 const localtime = require('./lib/localtime');
 
 console.log(localtime());
-console.log(localtime('YYYY-MM-DDTThh-mm-ss'));
+console.log(localtime(undefined, 'YYYY-MM-DDThh-mm-ss'));
+console.log(localtime('', 'YYYY-MM-DDThh-mm-ss'));
+console.log(localtime('2016-11-08T01:57:16.642Z', 'YYYY/MM/DD hh:mm:ss'));
