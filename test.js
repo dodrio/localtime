67f833c9fa3abba1e1f5461fe0bee36b1970f5fd
m31271n <m31271n@users.noreply.github.com>
2016-11-08 16:18:58 +0800

    Update test.js

--- test.js
+++ test.js
@@ -6,3 +6,4 @@ console.log(localtime());
 console.log(localtime(undefined, 'YYYY-MM-DDThh-mm-ss'));
 console.log(localtime('', 'YYYY-MM-DDThh-mm-ss'));
 console.log(localtime('2016-11-08T01:57:16.642Z', 'YYYY/MM/DD hh:mm:ss'));
+console.log(localtime('2016-11-08T01:57:16.642Z', 'YYYY/MM/DD hh:mm'));
